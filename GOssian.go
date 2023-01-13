5b7224e16c61005ffd77c27e43bbd711431f60ff
IgrecL <ngowg5p3v@relay.firefox.com>
2023-01-13 17:58:32 +0100

    Génération du masque et de la nouvelle image
    Co-authored-by: haddent <haddent@users.noreply.github.com>
    Co-authored-by: amFarwati <ElMagicarp@users.noreply.github.com>

--- GOssian.go
+++ GOssian.go
@@ -5,13 +5,16 @@ import (
 	"image"
 	"image/jpeg"
 	_ "image/jpeg"
+	"math"
 	"os"
 )
 
 func main() {
-	var img image.Image = importerImage("image.jpg")
+	var img image.Image = importerImage("test2.jpg")
+	rayon := 2
 	println(img)
-	genererMasque(3)
+	genererMasque(rayon, 1.0)
+	conversionImage(img, rayon)
 	// flouGaussien(img, 3)
 
 }
@@ -33,24 +36,83 @@ func importerImage(chemin string) image.Image {
 	return img
 }
 
-func genererMasque(rayon int) [][]int {
-	masque := make([][]int, 2*rayon+1)
+// Renvoie la valeur de loi normale à deux variables (x, y)
+func normpdf(x, y, sigma float64) float64 {
+	numerateur := -(x*x + y*y)
+	denominateur := 2 * sigma * sigma
+	return 1 / (2 * math.Pi * sigma * sigma) * math.Pow(math.E, (numerateur/denominateur))
+}
+
+func genererMasque(rayon int, sigma float64) [][]float64 {
+
+	// Initialisation du masque
+	masque := make([][]float64, 2*rayon+1)
 	for i := range masque {
-		masque[i] = make([]int, 2*rayon+1)
+		masque[i] = make([]float64, 2*rayon+1)
 	}
-	for i := range masque {
-		for j := range masque[i] {
-			fmt.Print(masque[i][j], " ")
+
+	// Remplissage du masque
+	for i := -rayon; i < rayon+1; i++ {
+		for j := -rayon; j < rayon+1; j++ {
+			masque[i+rayon][j+rayon] = normpdf(float64(i), float64(j), sigma)
 		}
-		fmt.Println()
 	}
+
 	return nil
 }
 
-func flouGaussien(img image.Image, rayon int) {
-	dim := img.Bounds()
-	imageRVB := image.NewRGBA(image.Rect(0, 0, dim.Dx(), dim.Dy()))
-	print(imageRVB)
+func flouGaussien(img image.RGBA, rayon int) {
+
+}
+
+func conversionImage(img image.Image, rayon int) {
+	largeur, hauteur := img.Bounds().Dx(), img.Bounds().Dy()
+
+	// Initialisation du tableau 3D avec un contour
+	new := make([][][]float64, largeur+2*rayon)
+	for k := range new {
+		new[k] = make([][]float64, hauteur+2*rayon)
+		for i := 0; i < len(new); i++ {
+			for j := 0; j < len(new[i]); j++ {
+				new[i][j] = make([]float64, 3)
+			}
+		}
+	}
+
+	// Remplissage du tableau 3D
+	for i := 0; i < largeur; i++ {
+		for j := 0; j < hauteur; j++ {
+			for k := 0; k < 3; k++ {
+				r, g, b, _ := img.At(i, j).RGBA()
+				new[i+rayon][j+rayon][0] = float64(r)
+				new[i+rayon][j+rayon][1] = float64(g)
+				new[i+rayon][j+rayon][2] = float64(b)
+			}
+		}
+	}
+
+	// On remplit le contour en appliquant l'image en miroir
+	for i := rayon; i < largeur+rayon; i++ {
+		for j := 0; j < rayon; j++ {
+			// Gauche et droite
+			new[i][j] = new[i][2*rayon-j]
+			new[i][rayon+hauteur+j] = new[i][rayon+hauteur-2-j]
+			// Haut et bas
+			new[j][i] = new[2*rayon-j][i]
+			new[rayon+largeur+j][i] = new[rayon+largeur-2-j][i]
+		}
+	}
+
+	for i := range new {
+		for j := range new[i] {
+			if new[i][j][0] == 0 {
+				fmt.Print("  0   ")
+			} else {
+				fmt.Print(new[i][j][0], " ")
+			}
+		}
+		fmt.Print("\n\n\n")
+	}
 
 }
 
